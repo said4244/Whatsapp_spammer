41467c251ec660eabaa32dea7cd61e6aec8a9a62
said4244 <69765135+said4244@users.noreply.github.com>
2020-12-02 22:52:06 +0100

    Update whatsapp_bomber.py

--- whatsapp_bomber.py
+++ whatsapp_bomber.py
@@ -5,10 +5,10 @@ import time
 def whatsapp_bomber():
     driver = webdriver.Chrome(r'C:\Users\User\Desktop\chromedriver.exe') #locatie van chromedriver*******
     driver.get('https://web.whatsapp.com')
-    name = input('Enter the name of user or group you want to send messages to : ')
-    msg = input('Enter the message here : ')
-    count = input('specify how many times you want to send the message : ')
-    input('Enter anything after scanning the QR code')
+    name = input('Naar wie wil je het bericht sturen? : ')
+    msg = input('Evul hier je bericht in : ')
+    count = input('hoevaak moet het bericht gestuurd worden? : ')
+    input('klik op een willekeurige toets, na het scannen van de QR code :')
     user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
     user.click()
     msg_box = driver.find_element_by_class_name('_3uMse')
